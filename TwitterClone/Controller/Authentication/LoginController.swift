5a418b16328449cb2a78afb78b68202aff36fc88
kim-yongdae <ydkim2110@gmail.com>
2021-04-03 14:30:03 +0900

    Update UI after authentication

--- TwitterClone/Controller/Authentication/LoginController.swift
+++ TwitterClone/Controller/Authentication/LoginController.swift
@@ -79,7 +79,13 @@ class LoginController: UIViewController {
                 return
             }
             
-            print("DEBUG : Successfully logged in...")
+            guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else { return }
+            
+            guard let tab = window.rootViewController as? MainTablController else { return }
+
+            tab.authenticateUserAndConfigureUI()
+            
+            self.dismiss(animated: true, completion: nil)
         }
     }
     
