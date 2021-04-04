f370daabf5028b76df9e059c09160282f37d204d
kim-yongdae <ydkim2110@gmail.com>
2021-04-04 16:08:51 +0900

    Search page UI

---
+++ TwitterClone/View/UserCell.swift
@@ -0,0 +1,58 @@
+//
+//  UserCell.swift
+//  TwitterClone
+//
+//  Created by 김용대 on 2021/04/04.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    private let profileImage: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 40, height: 40)
+        iv.layer.cornerRadius = 40 / 2
+        iv.backgroundColor = .twitterBlue
+        return iv
+    }()
+
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "Username"
+        return label
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Fullname"
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImage)
+        profileImage.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+        let stack = UIStackView(arrangedSubviews: [usernameLabel, fullnameLabel])
+        stack.axis = .vertical
+        stack.spacing = 2
+        
+        addSubview(stack)
+        stack.centerY(inView: self, leftAnchor: profileImage.rightAnchor, paddingLeft: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
