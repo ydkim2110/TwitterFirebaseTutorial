d82cd420e274244c5f8aa06d790ed717ba29fa02
kim-yongdae <ydkim2110@gmail.com>
2021-04-05 11:22:08 +0900

    Following user

--- TwitterClone/Utils/Constants.swift
+++ TwitterClone/Utils/Constants.swift
@@ -11,6 +11,8 @@ let DB_REF = Database.database().reference()
 let REF_USERS = DB_REF.child("twitter_users")
 let REF_TWEETS = DB_REF.child("twitter_tweets")
 let REF_USER_TWEETS = DB_REF.child("twitter_user_tweets")
+let REF_USER_FOLLOWERS = DB_REF.child("twitter_user_followers")
+let REF_USER_FOLLOWING = DB_REF.child("twitter_user_following")
 
 let STORAGE_REF = Storage.storage().reference()
 let STORAGE_PROFILE_IMAGES = STORAGE_REF.child("twitter_profile_image")
