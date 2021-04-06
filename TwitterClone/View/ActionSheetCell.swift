4524ec27ba0b4413033388ca0cc8cd219c55de88
kim-yongdae <ydkim2110@gmail.com>
2021-04-06 10:49:19 +0900

    Action sheet data model & view Model

--- TwitterClone/View/ActionSheetCell.swift
+++ TwitterClone/View/ActionSheetCell.swift
@@ -11,6 +11,10 @@ class ActionSheetCell: UITableViewCell {
     
     // MARK: - Properties
     
+    var option: ActionSheetOptions? {
+        didSet { configure() }
+    }
+    
     private let optionImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFit
@@ -44,5 +48,10 @@ class ActionSheetCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    // MARK: - Helpers
     
+    func configure() {
+        titleLabel.text = option?.description
+    }
 }
