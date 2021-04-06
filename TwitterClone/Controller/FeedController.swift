bf22860c33805eb3501901e455c2bdb488f25a6e
kim-yongdae <ydkim2110@gmail.com>
2021-04-06 15:46:00 +0900

    Setup Likes

--- TwitterClone/Controller/FeedController.swift
+++ TwitterClone/Controller/FeedController.swift
@@ -112,6 +112,14 @@ extension FeedController: UICollectionViewDelegateFlowLayout {
 // MARK : - TweetCellDelegate
 
 extension FeedController: TweetCellDelegate {
+    func handleLikeTapped(_ cell: TweetCell) {
+//        guard var tweet = cell.tweet else { return }
+
+        cell.tweet?.didLike.toggle()
+
+        print("DEBUG : Tweet is likes is \(cell.tweet?.didLike)")
+    }
+    
     func handleReplyTapped(_ cell: TweetCell) {
         guard let tweet = cell.tweet else { return }
         let controller = UploadTweetController(user: tweet.user, config: .reply(tweet))
