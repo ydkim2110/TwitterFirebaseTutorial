91f011a8290d8c405101bcfcce36fb5369e66eda
kim-yongdae <ydkim2110@gmail.com>
2021-04-08 09:48:32 +0900

    Fetching notification from firebase

--- TwitterClone/Controller/NotificationsController.swift
+++ TwitterClone/Controller/NotificationsController.swift
@@ -13,7 +13,11 @@ class NotificationsController: UITableViewController {
     
     // MARK: - Properties
     
-    private var notifications = [Notification]()
+    private var notifications = [Notification]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
     // MARK: - Lifecycle
     
@@ -21,6 +25,14 @@ class NotificationsController: UITableViewController {
         super.viewDidLoad()
         
         configureUI()
+        fetchNotifications()
+    }
+    
+    // MARK: - API
+    func fetchNotifications() {
+        NotificationService.shared.fetchNotifications { notifications in
+            self.notifications = notifications
+        }
     }
     
     // MARK: - Helpers
@@ -38,11 +50,12 @@ class NotificationsController: UITableViewController {
 extension NotificationsController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return notifications.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NotificationCell
+        
         return cell
     }
     
