4dbf528f51bfeb3587350875345c3e0217f2e6ed
kim-yongdae <ydkim2110@gmail.com>
2021-04-10 19:38:06 +0900

    Setup edit profile header

--- TwitterClone/Controller/EditProfileController.swift
+++ TwitterClone/Controller/EditProfileController.swift
@@ -12,6 +12,9 @@ class EditProfileController: UITableViewController {
     // MARK: - Properties
     
     private let user: User
+    private lazy var headerView = EditProfileHeader(user: user)
+
+    
     
     // MARK: - Lifecycle
     
@@ -27,6 +30,7 @@ class EditProfileController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationBar()
+        configureTableView()
     }
     
     // MARK: - Selectors
@@ -54,5 +58,19 @@ class EditProfileController: UITableViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(handleDone))
         navigationItem.rightBarButtonItem?.isEnabled = false
+    }
+    
+    func configureTableView() {
+        tableView.tableHeaderView = headerView
+        headerView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 180)
+        tableView.tableFooterView = UIView()
+        
+        headerView.delegate = self
+    }
+}
+
+extension EditProfileController: EditProfileHeaderDelegate {
+    func didTapChangeProfilePhoto() {
+        print("DEBUG : Handle change photo...")
     }
 }
